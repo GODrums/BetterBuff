5346bdd163fcfadab34c46a639659fb7e98cac7a
GODrums <armin.stanitzok@outlook.de>
2024-01-27 23:47:43 +0100

    Adjust Sentry monitoring

--- src/lib/util/sentry.ts
+++ src/lib/util/sentry.ts
@@ -5,19 +5,17 @@ export function initSentry() {
 		dsn: "https://a35e0b6b92b50892d9daa4daa2992db2@o4506512325738496.ingest.sentry.io/4506516317536256",
 		release: `betterbuff@${browser?.runtime?.getManifest()?.version ?? '0.0.0'}`,
 		integrations: [
-		  new Sentry.BrowserTracing({
-			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-			tracePropagationTargets: ["localhost", /^https:\/\/*\.rums\.dev/],
-		  }),
+		  new Sentry.BrowserTracing(),
 		  new Sentry.Replay({
 			maskAllText: false,
 			blockAllMedia: false,
 		  }),
 		],
 		// Performance Monitoring
-		tracesSampleRate: 1.0,
+		tracesSampleRate: 0.2,
+		tracePropagationTargets: ["localhost", /^https:\/\/*\.rums\.dev/],
 		// Session Replay
-		replaysSessionSampleRate: 0.2,
-		replaysOnErrorSampleRate: 1.0,
+		replaysSessionSampleRate: 0,
+		replaysOnErrorSampleRate: 1,
 	  });
 }

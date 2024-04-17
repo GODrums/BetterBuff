d9186085efaebe038aaebfee74eebfd92e9a528e
GODrums <armin.stanitzok@outlook.de>
2024-04-18 00:59:47 +0200

    About page elements

--- src/lib/pages/NewPopup.svelte
+++ src/lib/pages/NewPopup.svelte
@@ -4,6 +4,7 @@
     import { ExtensionStorage } from '../util/storage';
     import BBlogo from '/icon/512.png';
     import BUlogo from '$lib/icons/bu-logo.png';
+    import JoinDiscord from '/join_discord.png';
     import * as Tabs from '$lib/components/ui/tabs/index.js';
     import { Button } from '$lib/components/ui/button';
     import SettingPaymentMethods from '$lib/components/SettingPaymentMethods.svelte';
@@ -11,6 +12,8 @@
     import ListingDifference from '../components/ListingDifference.svelte';
     import SettingListingOptions from '../components/SettingListingOptions.svelte';
     import { ScrollArea } from '$lib/components/ui/scroll-area';
+    import { DISCORD_URL } from '$lib/util/globals';
+    import Badge from '$lib/components/ui/badge/badge.svelte';
 
     let m = { x: 0, y: 0 };
 
@@ -18,6 +21,8 @@
         m.x = event.clientX;
         m.y = event.clientY;
     }
+
+    const version = browser.runtime.getManifest().version;
 </script>
 
 <div class="dark flex flex-col bg-card text-card-foreground justify-between h-[600px] w-[350px] overflow-hidden heroBG" on:mousemove={handleMousemove} role="document">
@@ -45,16 +50,29 @@
         <Tabs.Content value="about">
             <ScrollArea class="h-[488px] rounded-md">
                 <div class="flex flex-col items-center">
-                    <div class="flex items-center justify-center gap-2 pb-2">
-                        <p class="scroll-m-20 text-sm font-medium text-gray-600">based on</p>
-                        <Button variant="ghost" class="py-1 px-2 h-7" on:click={() => window.open('https://github.com/PenguiniVogel/BuffUtility')}>
-                            <img class="h-6" src={BUlogo} alt="BuffUtility" />
+                    <div class="flex justify-center pt-12">
+                        <img class="h-20" src={BBlogo} alt="BetterBuff" />
+                    </div>
+                    <h1 class="pt-2 text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">BetterBuff</h1>
+                    <div class="flex flex-col items-center justify-center gap-4 py-2">
+                        <div class="flex items-center gap-2">
+                            <p class="scroll-m-20 text-sm font-medium text-gray-600">based on</p>
+                            <Button variant="ghost" class="py-1 px-2 h-7" on:click={() => window.open('https://github.com/PenguiniVogel/BuffUtility')}>
+                                <img class="h-6" src={BUlogo} alt="BuffUtility" />
+                            </Button>
+                        </div>
+                        <Badge variant="secondary" class="border-muted text-base">BETA - Version {version}</Badge>
+                        <Button variant="ghost" class="p-0 py-4" on:click={() => window.open(DISCORD_URL)}>
+                            <img class="h-10" src={JoinDiscord} alt="Join us on Discord" />
                         </Button>
+
                     </div>
-                    <div class="relative">
-                        <img class="h-12" src={BBlogo} alt="BetterBuff" />
-                    </div>
-                    <h3 class="scroll-m-20 text-2xl font-bold tracking-tight">BetterBuff</h3>
+                    <footer class="absolute bottom-2 w-full text-center text-xs font-semibold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                        <p>
+                            Built with 🖤 in Munich by
+                            <span class="cursor-pointer text-green-900" on:click={() => window.open('https://github.com/GODrums')} role="link" tabindex="-1">Rums</span>
+                        </p>
+                    </footer>
                 </div>
             </ScrollArea>
         </Tabs.Content>

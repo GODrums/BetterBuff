b8f7a52397c00c6674b11e1b1b11caae170c2682
GODrums <armin.stanitzok@outlook.de>
2024-04-18 23:23:34 +0200

    Final Adjustments for release

--- src/lib/util/globals.ts
+++ src/lib/util/globals.ts
@@ -41744,4 +41744,4 @@ export const TIME_TO_TEXT = {
 };
 
 export const DISCORD_URL = 'https://discord.gg/VQWXp33nSW';
-export const GITHUB_URL = 'https://github.com/GODrums/BetterFloat';
+export const GITHUB_URL = 'https://github.com/GODrums/BetterBuff';
